6c54c09d2614fb9c5be935e7e99005b35f9d2625
NqDyK2 <nguyenquyduyabc@gmail.com>
2024-03-27 00:23:13 +0700

    Feat: Config routes and main layout

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,24 @@ import "./App.css"
 import { Counter } from "./features/counter/Counter"
 import { Quotes } from "./features/quotes/Quotes"
 import { Login } from "./features/auth/Login-UI"
+import { MainLayout } from "./layouts/MainLayout"
 import logo from "./logo.svg"
+import { Route, Routes } from "react-router-dom"
+import { Dashboard } from "./features/dashboard/Dashboard"
+// import { PrivateRouter } from "./components/auth/PrivateRouter"
 
 const App = () => {
   return (
     <div className="App">
-      <Login checkLogin={(user: string, pass: string) => {
-        return user === "user" && pass === "password"
-      }} />
-    </div>
+      <Routes>
+        <Route index path="" element={<Login checkLogin={(user: string, pass: string) => {
+          return user === "user" && pass === "password"
+        }} />} />
+        <Route path="main" element={<MainLayout />} >
+          <Route index path="" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </div >
   )
 }
 
